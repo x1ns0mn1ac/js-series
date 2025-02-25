413e293dbce5c7284c232a69eeb6e7098ca15f43
x1ns0mn1ac <neeravmandrah30@gmail.com>
2025-02-25 22:31:12 +0000

    about data types

---
+++ js_series_main/data.js
@@ -0,0 +1,15 @@
+"use strict"
+
+//alert( 2 +3) // only for browsers
+
+// int - number range 2 ^ ~52
+// string - either '' or ""
+// bigint - self explanatory, surpasses the int limit ig like float but integer idk why i wrote so f*ing long lmao
+// boolean - true false 
+// symbol - unique identifier
+// undefined - it is not defined and needs to be define
+// null - standalone value
+
+console.log(typeof undefined);
+console.log(typeof null) // null is object??? well learn soon enough
+
---
+++ js_series_main/variables.js
@@ -0,0 +1,13 @@
+const account_id = 14445532
+let account_email = "rizzlord@google.com"
+var account_password = "ihopenoonesgoingthroughmyfirstgitprojects"
+acccount_city = "Indore"
+let account_state;
+
+//account_id = 2
+account_email = "rizz@google.com"
+account_city = "rajpur"
+
+//let is preferred over var because it enable scope
+
+console.table([account_id, account_email, account_password, account_city, account_state])
