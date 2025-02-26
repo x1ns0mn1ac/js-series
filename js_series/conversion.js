ae88ae8cc61d04f2f528a6392b85144b899e5f70
x1ns0mn1ac <neeravmandrah30@gmail.com>
2025-02-26 22:53:18 +0000

    conversion of data types

---
+++ js_series/conversion.js
@@ -0,0 +1,18 @@
+// let score = 45
+
+// console.log(typeof score)
+
+// let runs = Number(score)
+// console.log(typeof runs)
+
+// string = nan
+// boolean = 1 or 0
+
+let trueorfalse = "sarvesh"
+
+let boolean = Boolean(trueorfalse)
+
+console.log(boolean)
+
+// if empty - false
+// if filled with anything - true
