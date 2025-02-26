530bbd9faf63620398d691c625f68a1c3ecc191c
x1ns0mn1ac <neeravmandrah30@gmail.com>
2025-02-26 23:14:28 +0000

    operations on data types

---
+++ js_series/operations.js
@@ -0,0 +1,21 @@
+// let value = 4
+// let negValue = -value
+
+// console.log(negValue)
+
+//console.log(2**8)
+
+// let str1 = "hello"
+// let str2 = "hitesh"
+
+// let str3 = str1 + str2
+
+// console.log(str3)
+
+console.log("1" + 2 + 3) // sab string mai convert hojatehe
+console.log(2 + 3 + "1") // 2+3 hota and then string gets added like a sentencee
+
+let gamenum = 200
+++gamenum; //prefix adds before returning, postfix adds after returning
+
+console.log(gamenum)
