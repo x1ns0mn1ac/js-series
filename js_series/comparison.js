ede687fe5fe8522d41357aa2918d7579c0c43172
x1ns0mn1ac <neeravmandrah30@gmail.com>
2025-02-27 06:35:10 +0000

    comparison b/w  data types

---
+++ js_series/comparison.js
@@ -0,0 +1,13 @@
+// console.log( 2 > 1 )
+
+// console.log("2" > 1)
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); // comparison and equality operator are different in operation
+
+console.log(undefined == 0); // false in  all three cases
+
+// avoid these types of comparisons for clean code
+
+
