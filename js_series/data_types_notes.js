bb49dffeae71b56d2c734dae6aa8a0f3d4f45386
x1ns0mn1ac <neeravmandrah30@gmail.com>
2025-02-27 07:39:15 +0000

    data type notes

---
+++ js_series/data_types_notes.js
@@ -0,0 +1,27 @@
+// two types of Data based on how data is Stored 
+
+// #primitive
+// 7 types 
+// string, number, boolean, null, undefined, symbol, BigInt
+
+// JS dynamically typed language
+
+// #reference non primitive
+// Array, objects, functions
+
+const id = Symbol('123)')
+const anotherid = Symbol('123')
+
+console.log(id === anotherid);
+
+const myArray = ["sdlfkds", "jdfdskf"];
+
+
+let myObj = {
+    name: "hitesh",
+    age: 123,
+}
+
+const myFunction = function(){
+    console.log("Hellow worldo");
+}
