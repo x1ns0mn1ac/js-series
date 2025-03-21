2953068267809392a41c2bb827d5feae6d4e8e50
x1ns0mn1ac <neeravmandrah30@gmail.com>
2025-03-21 20:07:26 +0000

    object de-structure and json

---
+++ js_series_2/objects3.js
@@ -0,0 +1,21 @@
+const course = {
+    coursename: "js in hindi",
+    prince: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstrucotr
+
+const {courseInstructor: instructor} = course // object destructure
+//console.log(courseInstructor);
+console.log(instructor);
+
+// arrays are also destructured but thats alot less common
+
+// { //json 
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+
