e18855848196b39ebfea40a255efa3eeaa1ed241
x1ns0mn1ac <neeravmandrah30@gmail.com>
2025-07-01 19:35:38 +0000

    loops

---
+++ js_series_4/iterators.js
@@ -0,0 +1,13 @@
+for (i = 1; i<=10; i++){
+    if (i == 5 || i == 7){
+        continue
+    }else if (i == 9){
+        break
+    }
+    
+    console.log(i)
+}
+
+// theres literally nothing to write about ts
+
+// also you can declare const inside these loops again and again and theyll be declared again somehow
---
+++ js_series_4/while.js
@@ -0,0 +1,6 @@
+
+i = 1
+do{
+    console.log(i)
+    i++
+} while(i<=5)
