cd7bddba147e537d3f450e04dcb318b29d4e3655
x1ns0mn1ac <neeravmandrah30@gmail.com>
2025-08-24 22:37:36 +0000

    BMI calculator

--- projects/bmi/chaiaurcode.js
+++ projects/bmi/chaiaurcode.js
@@ -5,9 +5,16 @@ form.addEventListener('submit', function(e){
 
     const height = parseInt(document.querySelector('#height').value)
     const weight = parseInt(document.querySelector('#weight').value)
-    const results = parseInt(document.querySelector('#results').value)
+    const results = document.querySelector('#results')
 
-    if(height = '' || height < 0 || isNaN(height)){
+    if(height === '' || height < 0 || isNaN(height)){
         results.innerHTML = `Please give a valid height ${height}`;
+    } else if(weight === '' || weight < 0 || isNaN(weight)){
+        results.innerHTML = `Please give a valid weight ${weight}`;
+    } else {
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+
+        results.innerHTML = `Your BMI is ${bmi}`;
     }
+    // results.innerHTML = `${height}`;
 })
